30eae59ab404d2311adeda8423057153877d5f7e
tgltts <g82651504@gmail.com>
2025-09-16 21:45:20 -0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 
-int main() {
-    char nome[50];
-    char sexo[10];
-    int idade;
-
-    printf("Digite o nome: ");
-    scanf("%49s", nome);
-
-    printf("Digite o sexo (masculino/feminino): ");
-    scanf("%9s", sexo);
-
-    printf("Digite a idade: ");
-    scanf("%d", &idade);
-
+void conferir(char sexo[], int idade, char nome[]) {
     if ((sexo[0] == 'f' || sexo[0] == 'F') && idade < 25) {
         printf("%s ACEITA\n", nome);
     } else {
         printf("%s NAO ACEITA\n", nome);
     }
+}
 
+
+int main(){
+    char nome[20];
+    char sexo[20];
+    int idade;
+    printf("Digite o nome: ");
+    scanf("%19s", nome);
+    
+    printf("Qual o sexo: ");
+    scanf("%19s", sexo);
+    
+    printf("Digite a idade: ");
+    scanf("%d", &idade);
+    
+    conferir(sexo,idade,nome);
+    
     return 0;
 }
